0d6318aad9b696af92b7c6b66ecdf07666062c7c
strandbygaard <strandbygaard@localhost>
2011-06-25 08:32:29 +0200

    Added crypto lib and structure for getUsers

--- scripts/Withings.api.js
+++ scripts/Withings.api.js
@@ -231,18 +231,53 @@ var withings = function () {
         if (publickey.length == 0 || publickey == "") {
             return false;
         }
-        
+
         return true;
     }
 
-    var getUser = function() {
-    }
+    var getUsers;
+    getUsers = function() {
+        function ProcessRequest() {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                if (xmlHttp.responseText == "Not found") {
+                    Console.log("Not found");
+                }
+                else {
+                    var info = eval("(" + xmlHttp.responseText + ")");
+                    Console.log(info.jsonData[ 0 ].cmname);
+                }
+            }
+        }
+
+        //http://wbsapi.withings.net/account?action=getuserslist&email=demo@withings.com&hash=a1e72bfe9e22c9930a0046e5363c6efa
+        //1. http://wbsapi.withings.net/once?action=get
+        var xmlHttp, onceUrl;
+        xmlHttp = null;
+        onceUrl = "http://www.google.dk";
+//        onceUrl = "http://wbsapi.withings.net/once?action=get";
+
+        xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = ProcessRequest;
+        xmlHttp.open("GET", onceUrl, false);
+        xmlHttp.send(null);
+        //2. Step 2: Make a string which is the concatenation of the lowercased email address, the MD5 hash of the
+        // password and the once separated with ":". Please note that all email addresses are supposed to be lowercase
+        // during the whole authentication and hashing process. In other words, you must lowercase any email address
+        // before using it, since the MD5 hashes of doe@domain.com and Doe@domain.com are different and hence will lead
+        // to authentication errors when trying to access the API.
+        Crypto.MD5();
+        // Step 3: Using, once again, your usual MD5 library, compute the MD5 of the string obtained in step 2.
+        // Step 4: Use a hexadecimal representation of the result of the MD5 hash as the hash parameter of the
+        // WBSAPI service.
+        // http://wbsapi.withings.net/account?action=getuserslist&email=demo@withings.com&hash=99eb3bc8555ca782fa4a0fda53bc8a1a
+    };
+
 
     // The public API of this wrapper API
     return {
         init: init,
         initialized: initialized,
-        getUser: getUser
+        getUsers: getUsers
 
     };
 }();
